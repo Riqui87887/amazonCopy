9c774d7f67b66456d4c3a1fb29fcab425eb70998
Anna719 <stelmakhanna2001@gmail.com>
2021-11-06 19:32:19 +0200

    your

--- src/Header.js
+++ src/Header.js
@@ -46,7 +46,7 @@ function Header() {
         
 
         <div className="header__option">
-          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineOne">your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
