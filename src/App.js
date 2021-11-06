a37f7dc7574e4b50900a4239509d6903385ef11e
Anna719 <stelmakhanna2001@gmail.com>
2021-11-06 19:21:30 +0200

    elements

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Orders from "./Orders";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
+import { Elementss } from "@stripe/react-stripe-js";
 
 const promise = loadStripe(
   "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
