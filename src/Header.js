08d9913938d1087fecdfd42f2228c8d3a4c8dcab
Anna719 <stelmakhanna2001@gmail.com>
2021-11-06 19:34:43 +0200

    optionBasket

--- src/Header.js
+++ src/Header.js
@@ -51,7 +51,7 @@ function Header() {
         </div>
 
         <Link to="/checkout">
-          <div className="header__optionbasket">
+          <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
               {basket?.length}
