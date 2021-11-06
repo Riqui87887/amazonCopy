ef1dfe3bf55dd3a38bb366c7cabdb12c66dacfa7
Anna719 <stelmakhanna2001@gmail.com>
2021-11-06 19:28:30 +0200

    logo

--- src/Header.js
+++ src/Header.js
@@ -19,7 +19,7 @@ function Header() {
     <div className="header">
       <Link to="/">
         <img
-          className="header__logo!"
+          className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
       </Link>
