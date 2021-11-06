87236674de113dbdcf9caba65642a71da3a89fb4
Anna719 <stelmakhanna2001@gmail.com>
2021-11-06 19:35:16 +0200

    home

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import Product from "./Product";
 
 function Home() {
   return (
-    <div className="homee">
+    <div className="home">
       <div className="home__container">
         <img
           className="home__image"
